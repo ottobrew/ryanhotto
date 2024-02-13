33a02c6d81dfb7900f9e75907016173295f9b474
ottobrew <26239254+ottobrew@users.noreply.github.com>
2024-02-12 23:28:41 -0600

    Delete v2 directory

--- v2/script.js
+++
@@ -1,8 +0,0 @@
-// NodeList
-const questions = document.querySelectorAll('section');
-
-questions.forEach((question) => {
-    question.addEventListener('click', () => {
-        question.classList.toggle('opened');
-    })
-});
