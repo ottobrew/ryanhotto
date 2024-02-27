4e99ce46dc444315b7ae7793f8490f40a2cb9c99
ottobrew <26239254+ottobrew@users.noreply.github.com>
2024-02-27 11:41:44 -0600

    Projects and minor updates

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ questions.forEach((question) => {
         question.classList.toggle('opened');
     })
 });
+
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);    
+    modal.style.display = "flex";
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+window.onclick = function(e) {
+    if (e.target.className === "modal") {
+        e.target.style.display = "none";
+    }
+};
